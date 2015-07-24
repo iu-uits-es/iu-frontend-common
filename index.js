478256dad6bce7ceefe744a9a8aa03ea972478b7
Andy Hill <athill@iu.edu>
2015-07-24 14:57:32 -0400

    try import then export pt 4

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-export { Navigation }  from './components/Navigation';
-// export Navigation;
-export { UserContextMenu } from './components/UserContextMenu';
+// export { Navigation }  from './components/Navigation';
+// // export Navigation;
+// export { UserContextMenu } from './components/UserContextMenu';
+
+module.exports = require('./components/Navigation');
